4b3a4e4805b242851ce110a79a1c595e66e033bc
MrKay12 <quentin.lepoutre12@gmail.com>
2026-06-09 06:44:54 +0000

    feat: implement palindrome check and list reversal functions

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * reverse_list - reverses a singly linked list
+ * @head: head of list
+ *
+ * Return: new head
+ */
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		head->next = prev;
+		prev = head;
+		head = next;
+	}
+
+	return (prev);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to head of list
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *second, *first;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
+
+	slow = *head;
+	fast = *head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (fast != NULL)
+		slow = slow->next;
+
+	second = reverse_list(slow);
+	first = *head;
+
+	while (second != NULL)
+	{
+		if (first->n != second->n)
+			return (0);
+
+		first = first->next;
+		second = second->next;
+	}
+
+	return (1);
+}
