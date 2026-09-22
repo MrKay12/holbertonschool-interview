cc511a73e2d74d75437c30f9ded4fc7ba43835d3
MrKay12 <quentin.lepoutre12@gmail.com>
2026-09-22 11:57:41 +0000

    feat: implement rainwater retention calculation and add README file

---
+++ rain/0-rain.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Calculate how much rainwater is retained between walls.
+"""
+
+
+def rain(walls):
+    """
+    Return the total amount of rainwater retained.
+    """
+    if not walls:
+        return 0
+
+    left = 0
+    right = len(walls) - 1
+
+    left_max = 0
+    right_max = 0
+
+    water = 0
+
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+
+    return water
